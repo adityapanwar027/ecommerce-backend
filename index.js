0e145d2daf255917cffb92d84cfa7ffe8bac425d
adityapanwar027 <adityapanwar027@gmail.com>
2026-05-31 22:47:40 +0530

    Initial ecommerce backend setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect MongoDB
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("E-Commerce Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
